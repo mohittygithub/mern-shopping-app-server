45917936b936f55805669f87349fac963d5489d6
mohittygithub <smartfriendmohit@gmail.com>
2021-06-26 17:12:30 +0530

    server set

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
-//hi
-<h1>123</h1>;
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const shortId = require("shortid");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+console.log("uri=>", process.env.MONGO_DB_URI);
+// mongo db config
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err.message));
+
+// server config
+const PORT = process.env.PORT || 50000;
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
